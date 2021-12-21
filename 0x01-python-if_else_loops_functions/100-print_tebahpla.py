1243c61e4c7e5903dfc844051b3ce75ff3485ad6
AngelPiva <angelpiva6@gmail.com>
2021-12-21 07:44:10 -0800

    fixed task 100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 n = 0
 for i in reversed(range(97, 123)):
-    print('{:c}'.format(i - n), end = '')
+    print('{:c}'.format(i - n), end='')
     if n == 0:
         n = 32
     else:

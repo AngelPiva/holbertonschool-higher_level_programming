8f35ea4f84daa004149b6868201cbc885b6a1c5d
AngelPiva <angelpiva6@gmail.com>
2021-12-22 21:11:48 -0800

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+if len(sys.argv) - 1 == 0:
+    print('{:d} arguments.'.format(len(sys.argv) - 1))
+elif len(sys.argv) - 1 == 1:
+    print('{:d} argument:'.format(len(sys.argv) - 1))
+else:
+    print('{:d} arguments:'.format(len(sys.argv) - 1))
+for i in range(1, len(sys.argv)):
+    print('{}: {}'.format(i, sys.argv[i]))

16d7f52a67d7076ede0a547f89b0822c2ee0295f
AngelPiva <angelpiva6@gmail.com>
2022-01-04 12:19:29 -0800

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if roman_string is None or not isinstance(roman_string, str):
+        return 0
     romans_n = [
             ["I", 1],
             ["V", 5],
@@ -10,8 +12,13 @@ def roman_to_int(roman_string):
             ["M", 1000]
             ]
     total = 0
+    actual = 0
     for i in roman_string:
         for x in romans_n:
             if i == x[0]:
-                total += x[1]
+                if actual == 0 or actual >= x[1]:
+                    total += x[1]
+                else:
+                    total += x[1] - (actual * 2)
+                actual = x[1]
     return total

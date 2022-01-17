0caa5c4701452a8f959981c552085cd39d860cf6
AngelPiva <angelpiva6@gmail.com>
2022-01-17 11:53:25 -0800

    task 100

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,6 +4,8 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
+    except (TypeError) as terror:
+        print("Exception: " + str(terror), file = sys.stderr)
+        return False
     except (ValueError) as verror:
         print("Exception: " + str(verror), file = sys.stderr)
-        return False

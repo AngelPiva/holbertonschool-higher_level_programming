d8e4c224e5b6cfb4040778856961215e823bed42
AngelPiva <angelpiva6@gmail.com>
2022-01-24 11:13:21 -0800

    fixed task 3

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""
+module that say your name
+"""
+
+
 def say_my_name(first_name, last_name=""):
+    """ function that say your name """
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:

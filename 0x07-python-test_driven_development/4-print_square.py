1514ac6bec177e26f872956011f98f4714e7975e
AngelPiva <angelpiva6@gmail.com>
2022-01-24 11:10:14 -0800

    task 4

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+module that prints a square with size size
+"""
+def print_square(size):
+    """ function that prints a square with size size """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for n in range(size):
+        for i in range(size):
+            print("#", end="")
+        print("")

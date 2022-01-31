b69ed2d5bb522d9eec24d97d6b421aae0c096ec4
AngelPiva <angelpiva6@gmail.com>
2022-01-31 13:45:37 -0800

    task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""
+module Only sub class of
+"""
+
+
 def inherits_from(obj, a_class):
-    if isinstance(obj, a_class) and \
-        issubclass(a_class, obj.__class__) is False:
+    """ returns True if the object is an instance of a class that inherited """
+    if isinstance(obj, a_class):
+        if issubclass(a_class, obj.__class__) is False:
             return True
     return False

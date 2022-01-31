e70eee3922f892dcea523dd454b5bd739741450d
AngelPiva <angelpiva6@gmail.com>
2022-01-31 12:20:07 -0800

    task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+module Same class or inherit from
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ returns True if obj is an instance of """
+    if isinstance(obj, a_class):
+        return True
+    return False

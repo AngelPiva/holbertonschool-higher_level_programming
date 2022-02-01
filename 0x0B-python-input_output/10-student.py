76d1cc54ac3cab6e24f9179e70d5d23a0a0a0f04
AngelPiva <angelpiva6@gmail.com>
2022-02-01 15:01:46 -0800

    taask 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+module
+"""
+
+
+class Student:
+    """ class Student """
+    def __init__(self, first_name, last_name, age):
+        """ initializes the data """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ retrieves a dictionary representation of a Student instance """
+        attr_cont = {}
+        if type(attrs) is list:
+            for elem in attrs:
+                if type(elem) is not str:
+                    return self.__dict__
+                if elem in self.__dict__:
+                    attr_cont[elem] = self.__dict__[elem]
+            return attr_cont 
+        return self.__dict__

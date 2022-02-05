26b12b2bd3ff3abcc830d745d4f7b99915600068
AngelPiva <angelpiva6@gmail.com>
2022-02-05 12:09:07 -0800

    task 13

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -101,3 +101,8 @@ class Rectangle(Base):
             for k, v in kwargs.items():
                 if k in attrs:
                     setattr(self, k, v)
+
+    def to_dictionary(self):
+        """ returns the dictionary representation of a Rectangle """
+        return {"id": self.id, "width": self.width, "height":
+                self.height, "x": self.x, "y": self.y}

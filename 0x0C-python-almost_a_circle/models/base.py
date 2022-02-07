45d2f1aa8e354d13677e3b44ed3082feb7c0ac54
AngelPiva <angelpiva6@gmail.com>
2022-02-07 09:25:26 -0800

    task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -25,7 +25,7 @@ class Base:
     def to_json_string(list_dictionaries):
         """ returns the JSON string representation of list_dictionaries """
         if len(list_dictionaries) == 0 or list_dictionaries is None:
-            return []
+            return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod

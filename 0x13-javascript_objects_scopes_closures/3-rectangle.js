ac956b5871f96a6b095e15b21968d1a63405dddc
AngelPiva <angelpiva6@gmail.com>
2022-04-05 13:11:44 -0700

    task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      [this.width, this.height] = [w, h];
+    }
+  }
+
+  print () {
+    for (let n = 0; n < this.height; n++) {
+      for (let i = 0; i < this.width; i++) process.stdout.write('X');
+      console.log('');
+    }
+  }
+};

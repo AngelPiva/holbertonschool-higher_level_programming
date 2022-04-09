02ae5e83ef8d7b2ce09004064f8fec90e5dcf664
AngelPiva <angelpiva6@gmail.com>
2022-04-09 13:05:39 -0700

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,10 +12,10 @@ if __name__ == '__main__':
                                 password=argv[2], port=3306, database=argv[3])
     cursor = con_datab.cursor()
     cursor.execute("SELECT * FROM states \
-                    ORDER BY states.id ASC;")
+                    WHERE name LIKE BINARY '{}' \
+                    ORDER BY states.id ASC;".format(argv[4]))
     elems = cursor.fetchall()
 
     for elem in elems:
-        if elem[1] == argv[4]:
             print(elem)
     con_datab.close()

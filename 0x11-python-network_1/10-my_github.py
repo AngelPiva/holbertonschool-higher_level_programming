7aab4ae90da1f002312f7cabe67e3a4d87a68775
AngelPiva <angelpiva6@gmail.com>
2022-04-24 22:54:57 +0000

    task 9

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" 9. My GitHub! """
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    r = requests.get("https://api.github.com/user", auth=(argv[1], argv[2])).json()
+    print(r.get("id"))

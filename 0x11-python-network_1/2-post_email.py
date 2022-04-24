3622423056972c68d3c72044b772cbc51e168aa3
AngelPiva <angelpiva6@gmail.com>
2022-04-24 20:18:31 +0000

    task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" 2. POST an email #0 """
+from sys import argv
+import urllib
+
+
+if __name__ == "__main__":
+    data = urllib.parse.urlencode({"email": argv[2]})
+    data = data.encode('ascii')
+    with urllib.request.urlopen(argv[1], data) as response:
+        print("{}".format(response.read().decode("utf-8")))

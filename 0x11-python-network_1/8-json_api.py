0d1cabcb4ea77f44d19b4ce997b62978cc447b9e
AngelPiva <angelpiva6@gmail.com>
2022-04-24 22:22:38 +0000

    task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Search API """
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    if len(argv) > 1:
+        q = argv[1]
+    else:
+        q = ""
+    r = requests.post("http://0.0.0.0:5000/search_user", data={"q": q})
+    resp = r.json()
+    try:
+        if resp:
+            print("[{}] {}".format(resp["id"], resp["name"]))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")

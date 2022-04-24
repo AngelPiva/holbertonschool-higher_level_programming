8aa055cd3906e08a06887f0a3b2a5c65f269158a
AngelPiva <angelpiva6@gmail.com>
2022-04-24 21:22:08 +0000

    task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" 5. Response header value #1 """
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    r = requests.get(argv[1])
+    print("{}".format(r.headers["X-Request-Id"]))

346955077c19486fc40e51f47c937ed866985c05
AngelPiva <angelpiva6@gmail.com>
2022-05-18 00:16:09 +0000

    task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,7 +2,6 @@
 
 const request = require('request');
 
-request(process.argv[2], function (error, response) {
-  console.log('code: ' + response.statusCode);
+request(process.argv[2], function (_error, response) {
+  console.log('code: ' + response.statusCode); // Print the response status code if a response was received
 });
-

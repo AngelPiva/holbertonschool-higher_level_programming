6af43924efdcda65c14e6fbf53a39899845d02e8
AngelPiva <angelpiva6@gmail.com>
2022-05-18 01:34:27 +0000

    task 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const file = process.argv[3];
+
+request(url, function (_error, _response, body) {
+  fs.writeFile(file, body, 'utf-8', function (error) {
+    if (error) {
+      console.log(error);
+    }
+  });
+});

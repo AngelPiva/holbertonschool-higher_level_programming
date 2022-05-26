323c7890fa2b47a26b7fa75b6c93825f8f73c3b3
AngelPiva <angelpiva6@gmail.com>
2022-05-25 21:40:19 -0300

    semistandard fix

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,10 +1,10 @@
 $.ajax({
-    url: 'https://swapi-api.hbtn.io/api/films/?format=json',
-    dataType: 'json'
-}).done(function(data) {
-    const movies = data.results;
+  url: 'https://swapi-api.hbtn.io/api/films/?format=json',
+  dataType: 'json'
+}).done(function (data) {
+  const movies = data.results;
 
-    for (let m = 0; m < movies.length; m++) {
-        $('UL#list_movies').append('<li>' + movies[m].title + '</li>');
-    }
+  for (let m = 0; m < movies.length; m++) {
+    $('UL#list_movies').append('<li>' + movies[m].title + '</li>');
+  }
 });
--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,6 +1,6 @@
 $.ajax({
-    url: 'https://fourtonfish.com/hellosalut/?lang=fr',
-    dataType: 'json'
-}).done(function(data) {
-    $('DIV#hello').text(data.hello);
+  url: 'https://fourtonfish.com/hellosalut/?lang=fr',
+  dataType: 'json'
+}).done(function (data) {
+  $('DIV#hello').text(data.hello);
 });
